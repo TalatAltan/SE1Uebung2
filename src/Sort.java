a9db52f6f5ca7d35ad1e5cbf18ff373fba37d662
TalatAltan <te4ltan@gmail.com>
2019-04-11 17:40:26 +0200

    Array length not longer limited

--- src/Sort.java
+++ src/Sort.java
@@ -1,12 +1,31 @@
 
 public class Sort {
-	private String[][] sequenceOrder = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+	private static String[][] sequenceOrder = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
 
-	public boolean isWellSorted(String[] sequence) {
+	public static boolean isWellSorted(String[] sequence) {
 		
-		if(sequence == null || sequence.length != 2) {
+		if(sequence == null) {
 			return false;
 		}	
+		
+		for (int x = sequence.length - 1; x > 0; x--)
+        {
+            for (int y = x - 1; y >= 0; y--)
+            {
+                String[] pair = new String[] {sequence[y], sequence[x]};
+
+                for (int i = 0; i < sequenceOrder.length; i++)
+                {
+                    if (pair[0] == sequenceOrder[i][1])
+                    {
+                        if (pair[1] == sequenceOrder[i][0])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
 		
 		for(int i = 0; i < sequenceOrder.length; i++) {
 			if(sequence[0] == sequenceOrder[i][1]) {
