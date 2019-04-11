d5d39e4c4acc1da071ae317b23022fbac7391b9f
TalatAltan <te4ltan@gmail.com>
2019-04-11 12:11:00 +0200

    implemented isWellSorted

--- src/Sort.java
+++ src/Sort.java
@@ -1,4 +1,22 @@
 
 public class Sort {
+	private String[][] sequenceOrder = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
 
+	public boolean isWellSorted(String[] sequence) {
+		
+		if(sequence == null || sequence.length != 2) {
+			return false;
+		}	
+		
+		for(int i = 0; i < sequenceOrder.length; i++) {
+			if(sequence[0] == sequenceOrder[i][1]) {
+				if(sequence[1] == sequenceOrder[i][0]) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
 }
