7881ceb93acc6d614de95e686c087397607cc73d
TalatAltan <te4ltan@gmail.com>
2019-04-16 22:02:23 +0200

    Refactor and dependence fix

--- src/Sort.java
+++ src/Sort.java
@@ -1,56 +1,40 @@
-
 public class Sort {
 	private static String[][] sequenceOrder = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
 
-	 public static boolean isWellSorted(String[] sequence)
-     {
-         if (sequence == null)
-         {
-             return false;
-         }
+	   public static boolean isWellSorted(String[] sequence)
+       {
+           if (sequence == null)
+           {
+               return false;
+           }
 
-         for (int x = sequence.length - 1; x > 0; x--)
-         {
-             for (int y = x - 1; y >= 0; y--)
-             {
-                 String[] pair = new String[] { sequence[y], sequence[x] };
-                 if (!isWellSortedPair(pair))
-                 {
-                     return false;
-                 }
-             }
-         }
-         return true;
-     }
+           for(int x = sequence.length -1; x >= 0; x--)
+           {
+               for (int y = 0; y < sequenceOrder.length; y++)
+               {
+                   if (sequence[x] == sequenceOrder[y][1])
+                   {
+                       if(!containsDependence(sequence, x, sequenceOrder[y][0]))
+                       {
+                           return false;
+                       }
+                   }
+               }
+           }
 
-     private static boolean isWellSortedPair(String[] pair)
-     {
-         for (int i = 0; i < sequenceOrder.length; i++)
-         {
-             if (pair[0] == sequenceOrder[i][0] && pair[1] == sequenceOrder[i][1])
-             {
-                 return true;
-             }
-         }
+           return true;
+       }
 
-         for (int i = 0; i < sequenceOrder.length; i++)
-         {
-             if (pair[0] == sequenceOrder[i][1])
-             {
-                 if (pair[1] == sequenceOrder[i][0])
-                 {
-                     return false;
-                 }
-                 else
-                 {
-                     if (!isWellSortedPair(new String[] { pair[0], sequenceOrder[i][0]})){
-                         return false;
-                     }
-                 }
-             }
-         }
+       private static boolean containsDependence(String[] sequence, int index, String req)
+       {
+           for (int i = index - 1; i >= 0; i--)
+           {
+               if (sequence[i] == req)
+               {
+                   return true;
+               }
+           }
 
-         return true;
-     }
-	
+           return false;
+       }
 }
--- src/TestSort.java
+++ src/TestSort.java
@@ -1,17 +1,86 @@
 import static org.junit.jupiter.api.Assertions.*;
-
+ 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+ 
 class TestSort {
-
+	String[][] a;
+	String[][] b;
+	String[][] c;
+	String[][] d;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		a= new String[][] {{""},{"A"},{"B"},{"C"},{"D"}};
+		b= new String[][] {{"A","A"},{"A","B"},{"B","C"},{"C","D"},{"D","A"},{"C","A"}};
+		c= new String[][] {{"A","B","C"},{"B","A","C"},{"D","C","B"},{"C","D","B"}};
+		d= new String[][] {{"A","B","C","D"},{"B","B","A","C"},{"D","C","B","A"},{"A","C","B","D"}};
 	}
-
+		
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		isWellSorted_Null_ReturnsFalse();
+		isWellSorted_0ElementArray_ReturnsTrue();
+		isWellSorted_1ElementArray_ReturnsTrue();
+		isWellSorted_1ElementArray_ReturnsFalse();
+		isWellSorted_2ElementArray_ReturnsTrue();
+		isWellSorted_2ElementArray_ReturnsFalse();
+		isWellSorted_3ElementArray_ReturnsTrue();
+		isWellSorted_3ElementArray_ReturnsFalse();
+		isWellSorted_4ElementArray_ReturnsTrue();
+		isWellSorted_4ElementArray_ReturnsFalse();
 	}
-
+	
+	void isWellSorted_Null_ReturnsFalse() {
+		assertFalse(Sort.isWellSorted(null));
+	}
+	
+	void isWellSorted_0ElementArray_ReturnsTrue() {
+		assertTrue(Sort.isWellSorted(a[0]));
+	}
+	
+	void isWellSorted_1ElementArray_ReturnsTrue() {
+		assertTrue(Sort.isWellSorted(a[1]));
+		assertTrue(Sort.isWellSorted(a[2]));
+	}
+	
+	void isWellSorted_1ElementArray_ReturnsFalse() {
+		assertFalse(Sort.isWellSorted(a[3]));
+		assertFalse(Sort.isWellSorted(a[4]));		
+	}
+	
+	void isWellSorted_2ElementArray_ReturnsTrue() {
+		assertTrue(Sort.isWellSorted(b[0]));
+		assertTrue(Sort.isWellSorted(b[1]));
+		assertTrue(Sort.isWellSorted(b[2]));
+		assertTrue(Sort.isWellSorted(b[3]));
+	}
+	
+	void isWellSorted_2ElementArray_ReturnsFalse() {
+		assertFalse(Sort.isWellSorted(b[4]));
+		assertFalse(Sort.isWellSorted(b[5]));		
+	}
+	
+	void isWellSorted_3ElementArray_ReturnsTrue() {
+		assertTrue(Sort.isWellSorted(c[0]));
+		assertTrue(Sort.isWellSorted(c[1]));
+	}
+	
+	void isWellSorted_3ElementArray_ReturnsFalse() {
+		assertFalse(Sort.isWellSorted(c[2]));
+		assertFalse(Sort.isWellSorted(c[3]));	
+		
+	}
+	
+	void isWellSorted_4ElementArray_ReturnsTrue() {
+		assertTrue(Sort.isWellSorted(d[0]));
+		assertTrue(Sort.isWellSorted(d[1]));
+	}
+	
+	void isWellSorted_4ElementArray_ReturnsFalse() {
+		assertFalse(Sort.isWellSorted(d[2]));
+		assertFalse(Sort.isWellSorted(d[3]));	
+		
+	}
+ 
 }
